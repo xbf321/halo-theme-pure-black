aba4aa483dc6e26a40cee59958fc135e256f9b2d
xingbaifang <xingbaifang@meituan.com>
2023-11-02 11:54:08 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-VERSION="0.0.2"
-
-FILE_NAME="halo-theme-pure-black-v${VERSION}.tar.gz"
-tar --exclude './node_modules/**' \
-  --exclude './src/**' \
-  --exclude './.gitignore' \
-  --exclude './package-lock.json' \
-  --exclude './package.json' \
-  --exclude './build.sh' \
-  --exclude './postcss.config.js' \
-  --exclude './tailwind.config.js' \
-  --exclude './vite.config.js' \
- -cvpzf $FILE_NAME ./**
+FILE_NAME="theme-pure-black.zip"
+zip -r $FILE_NAME ./** \
+  -i './templates/**' \
+  -i './README.md' \
+  -i './settings.gradle' \
+  -i './settings.yaml' \
+  -i './theme.yaml'
